00577d95ed9fbe52a46c9e53afe7a2f5d87afcb1
Atul Patil <atuluttampatil.1757@gmail.com>
2022-02-24 00:08:33 +0530

    🧪: add tests

--- scripts/run.js
+++ scripts/run.js
@@ -2,19 +2,20 @@ const main = async () => {
    
   const [owner, randomPerson] = await hre.ethers.getSigners();  
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("dob");
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  const txn = await domainContract.register("test");
+  let txn = await domainContract.register("test",  {value: hre.ethers.utils.parseEther('0.3')});  
   await txn.wait();
 
   const domainOwner = await domainContract.getAddress("test");
   console.log("Owner of domain:",domainOwner);
 
-  txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
-  await txn.wait();
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  
 };
 
 const runMain = async () => {
