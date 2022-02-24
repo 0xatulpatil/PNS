4f452a7382896cf9159256d36fcb85298b6d11fb
Atul Patil <atuluttampatil.1757@gmail.com>
2022-02-24 23:11:57 +0530

    🧪: add tests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,25 @@
 const main = async () => {
-   
-  const [owner, randomPerson] = await hre.ethers.getSigners();  
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
   const domainContract = await domainContractFactory.deploy("pns");
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
-  let txn = await domainContract.register("test",  {value: hre.ethers.utils.parseEther('0.3')});  
+  
+	let txn = await domainContract.register("atul",  {value: hre.ethers.utils.parseEther('0.1')});
+	await txn.wait();
+  console.log("Minted domain atul.pns");
+
+  txn = await domainContract.setRecord("beep boop beep, it Works!");
   await txn.wait();
+  console.log("Set record for atul.pns");
 
-  const domainOwner = await domainContract.getAddress("test");
-  console.log("Owner of domain:",domainOwner);
+  const address = await domainContract.getAddress("atul");
+  console.log("Owner of domain atul:", address);
 
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
-  
-};
+}
 
 const runMain = async () => {
   try {
